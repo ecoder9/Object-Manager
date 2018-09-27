ab6d090481cac9bf4e9c4ac46bbd2978fdad4972
Eric Marasco <43547799+ecoder9@users.noreply.github.com>
2018-09-27 08:00:42 -0700

    Update object.js
    Revised code to add new features and efficiency

--- object.js
+++ object.js
@@ -1,99 +1,147 @@
 function _obj()
 {
-	var _self = this;
 	
 	// OBJECT INTIALIZATION FROM OBJECT OR ARRAY
 	this.init = function(v){
-		if(v.constructor === Array)
+				
+		if(this.tof(v,"a") && v.length > 0)
 		{
 			for(i=0;i<v.length;i++)
 			{
-				if(typeof v[i] === "string")
+				if(this.tof(v[i],"s"))
 				{
-					_self[v[i]] = null;
+					this[v[i]] = null;
 				}
 			}
+			return {result: true};
 		}
-		else if(v.constructor === Object)
+		else if(this.tof(v,"o") && this.props(v).length > 0)
 		{
 			for(var key in v)
 			{
-				_self[key] = v[key];
+				this[key] = v[key];
 			}
+			return {result: true};
 		}
 		else
 		{
-			console.log("Invalid data type");
+			return {result: false, error: "Invalid data type"};
 		}
 	}
 	
 	// RETREIVE OBJECT DATA WITHOUT FUNCTIONS
-	this.data = function()
+	this.attr = function()
 	{
-		var attr = {}, k = _self.keys();
+		var a = {}, k = this.keys();
    		for(i=0;i<k.length;i++)
 			{
-				attr[k[i]] = this[k[i]];
+				a[k[i]] = this[k[i]];
 			}
-   		return attr;
+   		return a;
 	}
 	
-	// RESET AN ARRAY OF PROPERTIES
+	// FIND IF KEY AND VALUE PAIR MATCH
+	this.compare = function(k,v) {
+		var r;
+		if(this.tof(k,"s") && this.tof(v,"s")){
+			if(this.has(k))
+			{
+				r = (this[k] === v) ? {result: true} : {result: false, value: this[k]}
+			}
+			else
+			{
+				r = {result: false, error: "Property does not exist"}
+			}
+		}
+		else
+		{
+			r = {result: false, error: "Invalid parameter"}
+		}
+		return r;
+	}
+	
+	// REMOVE AN ARRAY OF PROPERTIES
 	this.destroy = function(arr)
 	{
-		if(arr.constructor === Array)
+		var r = [];
+		arr = (arr == null) ? this.keys() : arr;
+	
+		if(this.tof(arr,"a"))
 		{
 			for(i=0;i<arr.length;i++)
 			{
-				if(_self.exist(arr[i]))
-				{
-					_self.remove(arr[i]);
-				}	
-				else
-				{
-					console.log(arr[i] + " does not exist");
-				}
+				(this.has(arr[i])) ? this.remove(arr[i]) : r.push({index: i, key: arr[i]});
 			}
 		}
+		else
+		{
+			r = {result: false, error: "Array was not passed"}
+		}
+		
+		if(arr.length == 0) r = {result: true}
+		
+		return r;
 	}
 	
 	// CHECK IF PROPERTY EXISTS
-	this.exist = function(prop)
+	this.has = function(prop)
 	{
-		return _self.hasOwnProperty(prop);
+		return this.hasOwnProperty(prop);
 	}
 	
-	// RETREIVE OBJECT KEYS WITHOUT FUNCTIONS
+	// RETREIVE PROPERTIES FROM "THIS" OBJECT
 	this.keys = function()
 	{
-   		var keys = [];
-   		for(var key in this)
+   		return this.props(this);
+	}
+	
+	// GET ALL PROPERTIES FROM SPECIFIED OBJECT
+	this.props = function(v)
+	{
+		var prop = [];
+   		for(var key in v)
 			{
-				if(typeof _self[key] != "function") 
-				{
-					keys.push(key);
-				}
+				if(!this.tof(v[key],"f")) prop.push(key);
 			}
-   		return keys;
+   		return prop;		
 	}
 	
 	// REMOVE AND OBJECT PROPERTY THAT IS NOT A FUNCTION
 	this.remove = function(prop)
 	{
-		if(typeof _self[prop] != "function") 
+		if(!this.tof(this[prop],"f"))
 		{
-			delete _self[prop];
+			delete this[prop];
 		}
-		else
-		{
-			console.log("Unable to remove property");
-		}
+		return {result: (!this.has(prop))};
 	}
 	
 	// GET SIZE OF OBJECT WITHOUT FUNCTIONS
 	this.size = function()
 	{
-		return _self.keys().length;
+		return this.keys().length;
 	}
-
+	
+	// GET TYPEOF VARIABLE
+	this.tof = function(v,type) 
+	{ 
+		switch(type)
+		{
+			case "f":
+				return (typeof v === "function") ? true : false;
+			break;
+			case "s":
+				return (typeof v === "string") ? true : false;
+			break;
+			case "o":
+				return (v.constructor === Object) ? true : false;
+			break;
+			case "a":
+				return (v.constructor === Array) ? true : false;
+			break;
+			default:
+				return null;
+			break;
+		}
+	}	
 }
